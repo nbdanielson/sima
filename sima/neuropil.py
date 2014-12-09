06628d646296731292cc227e4ff59c1cdb7ba06b
Nathan Danielson <nbd2111@columbia.edu>
2014-12-09 00:23:00 -0500

    implementation of svoboda method for neuropil subtraction code is complete but not fully tested

--- sima/neuropil.py
+++ sima/neuropil.py
@@ -13,68 +13,88 @@ def subtract_neuropil(imSet, channel, label, kwargs):
     rois = ROIList(signals[label]['rois'])
 
     # Set mask parameter defaults
-    neuropil_mask_params = {'min_distance': 0, 'max_distance': None}
-                            # 'buffer_rois': False}
-    for param, value in kwargs.iteritems():
-        if param in neuropil_mask_params:
-            neuropil_mask_params[param] = value
+    params = {'min_distance': 0, 'max_distance': None, 'buffer_rois': True,
+              'contamination_ratio': 0.5}
 
-    nonROI_mask = []
-    for plane in range(len(rois[0].mask)):
+    for param, value in kwargs.iteritems():
+        if param in params:
+            params[param] = value
+
+    nonROI_mask = []  # one level for each z-plane
+    for plane in xrange(len(rois[0].mask)):
         nonROI_mask.append(np.ones(rois[0].mask[plane].todense().shape))
     nonROI_mask = np.array(nonROI_mask)
 
+    # Create the non-ROI mask
     for roi in rois:
-        for plane in range(len(roi.mask)):
-            nonROI_mask -= roi.mask[plane].todense()
-            nonROI_mask[nonROI_mask < 0] = 0
+        for plane in xrange(len(roi.mask)):
+            nonROI_mask[plane] -= roi.mask[plane].todense()
+        nonROI_mask[nonROI_mask < 0] = 0
+    if params['buffer_rois']:
+        for plane in xrange(len(nonROI_mask)):
+            inv_mask = ~nonROI_mask[plane].astype(bool)
+            for iteration in xrange(params['min_distance']):
+                inv_mask = ndimage.binary_dilation(inv_mask)
+            nonROI_mask[plane] = (~inv_mask).astype(float)
 
-    for roi, raw_signal in it.izip(rois, raw_signal):
-        roi_mask = []
-        for plane in range(len(roi.mask)):
-            roi_mask.append(roi.mask[plane].todense())
-        roi_mask = np.array(roi_mask)
+    neuropil_rois = []
+    # One neuropil mask for all ROIs
+    if params['max_distance'] is None and \
+            (params['min_distance'] == 0 or params['buffer_rois'] is True):
+        neuropil_rois.append(ROI(mask=nonROI_mask))
+    # Each ROI has a unique neuropil mask
+    else:
+        for roi in rois:
+            roi_mask = []
+            for plane in range(len(roi.mask)):
+                roi_mask.append(roi.mask[plane].todense())
+            roi_mask = np.array(roi_mask)
 
-        # Subtract the dilated ROI from the neuropil mask
-        dilated_mask = []
-        for plane_mask in roi_mask:
-            dilated_plane = plane_mask
-            for iteration in range(neuropil_mask_params['min_distance']):
-                dilated_plane = ndimage.binary_dilation(dilated_plane)
-            dilated_mask.append(dilated_plane)
+            # Subtract the dilated ROI from the neuropil mask if necessary
+            if not params['buffer_rois'] and params['min_distance'] > 0:
+                dilated_mask = []
+                for plane_mask in roi_mask:
+                    dilated_plane = plane_mask
+                    for iteration in range(params['min_distance']):
+                        dilated_plane = ndimage.binary_dilation(dilated_plane)
+                    dilated_mask.append(dilated_plane)
+                dilated_mask = np.array(dilated_mask)
+                neuropil_mask = nonROI_mask - dilated_mask
+                neuropil_mask[neuropil_mask < 0] = 0
+            else:
+                neuropil_mask = np.copy(nonROI_mask)
 
-        neuropil_mask = nonROI_mask - dilated_mask
-        neuropil_mask[neuropil_mask < 0] = 0
+            # Apply max distance threshold.
+            # Note this procedure enforces neuropil signal is taken only from
+            # planes on which the ROI is defined
+            if params['max_distance'] is not None:
+                include_mask = []
+                for plane_mask in roi_mask:
+                    include_plane = plane_mask
+                    for iteration in range(params['max_distance']):
+                        include_plane = ndimage.binary_dilation(include_plane)
+                    include_mask.append(include_plane)
+                include_mask = np.array(include_mask)
+                neuropil_mask *= include_mask
+            neuropil_rois.append(ROI(mask=neuropil_mask))
+    neuropil_rois = ROIList(neuropil_rois)
+    # TODO: enforce all neuropil ROIs are not empty?  Will cause extract error
 
-        # Apply max distance threshold
-        if neuropil_mask_params['max_distance'] is not None:
-            include_mask = []
-            for plane_mask in roi_mask:
-                include_plane = plane_mask
-                for iteration in range(neuropil_mask_params['max_distance']):
-                    include_plane = ndimage.binary_dilation(include_plane)
-            include_mask = np.array(include_mask)
-            neuropil_mask *= include_mask
+    # Calculate neuropil timecourse
+    neuropil_signals = extract_rois(imSet, neuropil_rois, channel)['raw']
 
-        neuropil_roi = ROIList([ROI(mask=neuropil_mask)])
+    # should be a list of 2D numpy arrays (rois x time)
+    corrected_timecourses = []
+    for seq_idx in xrange(len(raw_signals)):
+        if len(neuropil_signals[seq_idx]) == 1:
+            neuropil_signals[seq_idx] = \
+                [neuropil_signals[seq_idx][0]] * len(raw_signals[seq_idx])
 
-        # Calculate neuropil timecourse
-        neuropil_timecourse = extract_rois(imSet, neuropil_roi, channel)['raw']
-
-        # Iterate over roi pixels to calculate a weights mask
-        for plane_idx, plane in enumerate(roi.mask):
-            (x_coords, y_coords) = np.where(plane.todense())
-
-            weights = 0.
-            for x, y in it.izip(x_coords, y_coords):
-                for sequence_idx in xrange(len(raw_signal)):
-                    #TODO: MAKE SURE Y, X ORDER IS CORRECT
-                    imSet.sequences[sequence_idx][:, plane_idx, y, x, channel]
-
-                    #MAKE A NEW SLICED IMAGING DATASET OF 1 PIXEL, THEN EXTRACT A 1-PIXEL ROI! TO GET TIMECOURSE
-
-
-
-        # Extract a contamination timecourse (normalized sum of weights) * neuropil timecourse
-
-        # return a list (of lists?  Need to deal with cycles)
+        sequence_signals = []
+        for raw_timecourse, neuropil_timecourse in it.izip(
+                raw_signals, neuropil_signals):
+            sequence_signals.append(
+                raw_timecourse - params['contamination_ratio'] *
+                neuropil_timecourse)
+        corrected_timecourses.append(np.array(sequence_signals))
+    set_trace()

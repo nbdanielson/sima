e778db8be5107f8dbdd881cc96aab160c423c01f
Nathan Danielson <nbd2111@cumc.columbia.edu>
2015-02-01 17:05:35 -0500

    work on neuropil

--- sima/neuropil.py
+++ sima/neuropil.py
@@ -27,90 +27,97 @@ from pudb import set_trace
 
 def subtract_neuropil(imset, channel, label, min_distance=0, grid_dim=(3, 3),
                       contamination_ratio = 1):
-    # pulling out the raw imaging signals (one t-series per sequence per ROI)
-    signals = imset.signals(channel=channel)
-    raw_signals = signals[label]['raw']
-    rois = ROIList(signals[label]['rois'])
-    # mean_frame = np.squeeze(signals[label]['mean_frame'])
-    # OK
-    # Initialize mask (all True).  shape is zyx
-    nonROI_mask = []  # one level for each z-plane
-    for plane in xrange(len(rois[0].mask)):
-        nonROI_mask.append(np.ones(rois[0].mask[plane].todense().shape))
-    nonROI_mask = np.array(nonROI_mask)
-    # OK
-    # Create the non-ROI mask
-    neuropil_rois = []
-    # mean_neuropil_levels = np.zeros(grid_dim)
-    # mean_roi_levels = []
-    all_roi_mask = []
+    # # pulling out the raw imaging signals (one t-series per sequence per ROI)
+    # signals = imset.signals(channel=channel)
+    # raw_signals = signals[label]['raw']
+    # rois = ROIList(signals[label]['rois'])
+    # # mean_frame = np.squeeze(signals[label]['mean_frame'])
+    # # OK
+    # # Initialize mask (all True).  shape is zyx
+    # nonROI_mask = []  # one level for each z-plane
+    # for plane in xrange(len(rois[0].mask)):
+    #     nonROI_mask.append(np.ones(rois[0].mask[plane].todense().shape))
+    # nonROI_mask = np.array(nonROI_mask)
+    # # OK
+    # # Create the non-ROI mask
+    # neuropil_rois = []
+    # # mean_neuropil_levels = np.zeros(grid_dim)
+    # # mean_roi_levels = []
+    # all_roi_mask = []
 
-    assert len(rois[0].mask) == 1
+    # assert len(rois[0].mask) == 1
 
-    # TODO: Below is incorrect for multi-plane datasets and will fail!
-    for plane in xrange(len(rois[0].mask)):
-        for roi in rois:
-            roi_mask = np.array(roi.mask[plane].todense())
-            all_roi_mask.append(roi_mask)
-            # mean_roi_levels.append(
-            #     np.sum(roi_mask * mean_frame) / np.sum(roi_mask))
-        all_roi_mask = sum(all_roi_mask)
-        all_roi_mask = all_roi_mask.astype(bool)
-        dilated_mask = ndimage.binary_dilation(
-            all_roi_mask, iterations=min_distance)
-        nonROI_mask[plane] -= dilated_mask
+    # # TODO: Below is incorrect for multi-plane datasets and will fail!
+    # for plane in xrange(len(rois[0].mask)):
+    #     for roi in rois:
+    #         roi_mask = np.array(roi.mask[plane].todense())
+    #         all_roi_mask.append(roi_mask)
+    #         # mean_roi_levels.append(
+    #         #     np.sum(roi_mask * mean_frame) / np.sum(roi_mask))
+    #     all_roi_mask = sum(all_roi_mask)
+    #     all_roi_mask = all_roi_mask.astype(bool)
+    #     dilated_mask = ndimage.binary_dilation(
+    #         all_roi_mask, iterations=min_distance)
+    #     nonROI_mask[plane] -= dilated_mask
 
-    # Gets coordinates of ROI boundaries; bounds right and bottom inclusive
-        x = nonROI_mask[plane].shape[0]
-        y = nonROI_mask[plane].shape[1]
-        x_bounds = map(int, np.linspace(-1, x, grid_dim[0] + 1))
-        y_bounds = map(int, np.linspace(-1, y, grid_dim[1] + 1))
-        grid = [[np.zeros([x, y]) for i in xrange(grid_dim[0])]
-                for j in xrange(grid_dim[1])]
-        for i in xrange(len(x_bounds) - 1):
-            for j in xrange(len(y_bounds) - 1):
-                grid[i][j][x_bounds[i] + 1:x_bounds[i + 1],
-                           y_bounds[j] + 1:y_bounds[j + 1]] = 1
-                neuropil_mask = (grid[i][j] * nonROI_mask[plane]).astype(bool)
-                neuropil_rois.append(ROI(mask=neuropil_mask))
-                # mean_neuropil_levels[i, j] = np.sum(
-                #     neuropil_mask * mean_frame) / np.sum(neuropil_mask)
-        neuropil_rois = ROIList(neuropil_rois)
+    # # Gets coordinates of ROI boundaries; bounds right and bottom inclusive
+    #     x = nonROI_mask[plane].shape[0]
+    #     y = nonROI_mask[plane].shape[1]
+    #     x_bounds = map(int, np.linspace(-1, x, grid_dim[0] + 1))
+    #     y_bounds = map(int, np.linspace(-1, y, grid_dim[1] + 1))
+    #     grid = [[np.zeros([x, y]) for i in xrange(grid_dim[0])]
+    #             for j in xrange(grid_dim[1])]
+    #     for i in xrange(len(x_bounds) - 1):
+    #         for j in xrange(len(y_bounds) - 1):
+    #             grid[i][j][x_bounds[i] + 1:x_bounds[i + 1],
+    #                        y_bounds[j] + 1:y_bounds[j + 1]] = 1
+    #             neuropil_mask = (grid[i][j] * nonROI_mask[plane]).astype(bool)
+    #             neuropil_rois.append(ROI(mask=neuropil_mask))
+    #             # mean_neuropil_levels[i, j] = np.sum(
+    #             #     neuropil_mask * mean_frame) / np.sum(neuropil_mask)
+    #     neuropil_rois = ROIList(neuropil_rois)
 
-    # Calculate neuropil timecourse
-    neuropil_extracted = extract_rois(
-        imset, neuropil_rois, channel, remove_overlap=True)
-    neuropil_signals = neuropil_extracted['raw']
-    neuropil_smoothed = []
-    for seq_idx in xrange(len(neuropil_signals)):
-        df = pd.DataFrame(neuropil_signals[seq_idx].T)
+    # # Calculate neuropil timecourse
+    # neuropil_extracted = extract_rois(
+    #     imset, neuropil_rois, channel, remove_overlap=True)
+    # neuropil_signals = neuropil_extracted['raw']
+    # neuropil_smoothed = []
+    # for seq_idx in xrange(len(neuropil_signals)):
+    #     df = pd.DataFrame(neuropil_signals[seq_idx].T)
 
-        WINDOW_SIZE = 45
-        SIGMA = 5
-        neuropil_smoothed.append(pd.stats.moments.rolling_window(
-            df, window=WINDOW_SIZE, min_periods=WINDOW_SIZE / 2., center=True,
-            win_type='gaussian', std=SIGMA).values.T)
+    #     WINDOW_SIZE = 45
+    #     SIGMA = 5
+    #     neuropil_smoothed.append(pd.stats.moments.rolling_window(
+    #         df, window=WINDOW_SIZE, min_periods=WINDOW_SIZE / 2., center=True,
+    #         win_type='gaussian', std=SIGMA).values.T)
 
-    # should be a list of 2D numpy arrays (rois x time)
-    corrected_timecourses = []
-    # Calculate centroid of neuropil ROIs
-    neuropil_centroids = [[((x_bounds[i] + x_bounds[i + 1]) / 2.0,
-                            (y_bounds[j] + y_bounds[j + 1]) / 2.0)
-                          for j in xrange(len(y_bounds) - 1)]
-                          for i in xrange(len(x_bounds) - 1)]
+    # # should be a list of 2D numpy arrays (rois x time)
+    # corrected_timecourses = []
+    # # Calculate centroid of neuropil ROIs
+    # neuropil_centroids = [[((x_bounds[i] + x_bounds[i + 1]) / 2.0,
+    #                         (y_bounds[j] + y_bounds[j + 1]) / 2.0)
+    #                       for j in xrange(len(y_bounds) - 1)]
+    #                       for i in xrange(len(x_bounds) - 1)]
 
-    neuropil_centroids = np.array(neuropil_centroids)
-    roi_centroids = []
-    for roi in rois:
-        roi_centroid = np.array(roi.polygons.centroid.coords)[0]
-        roi_centroids.append(roi_centroid)
+    # neuropil_centroids = np.array(neuropil_centroids)
+    # roi_centroids = []
+    # for roi in rois:
+    #     roi_centroid = np.array(roi.polygons.centroid.coords)[0]
+    #     roi_centroids.append(roi_centroid)
+
+    f = np.load('/Users/Nathan/Desktop/neuropil_vars.npz')
+    raw_signals = f['raw_signals']
+    roi_centroids = f['roi_centroids']
+    neuropil_signals = f['neuropil_signals']
+    neuropil_smoothed = f['neuropil_smoothed']
+    neuropil_centroids = f['neuropil_centroids']
 
     set_trace()
 
     # Calculate correction factors
+    corrected_signals = []
     for seq_idx in xrange(len(raw_signals)):
         sequence_signals = []
-        set_trace()
         for raw_timecourse, roi_centroid in it.izip(
                 raw_signals[seq_idx], roi_centroids):
             distances = np.zeros(grid_dim)
@@ -126,11 +133,14 @@ def subtract_neuropil(imset, channel, label, min_distance=0, grid_dim=(3, 3),
             weighted_correction = map(
                 lambda x: weights * np.squeeze(x),
                 np.dsplit(correction, len(neuropil_smoothed[seq_idx][0, :])))
+
+            # TODO: SOMETHING IS NOT CALCULATING RIGHT -- CORRECTION_FACTORS IS
+            # CENTERED ABOUT 3 (INSTEAD OF 1)
             correction_factors = np.array(
                 [np.sum(weighted_correction[i])
                  for i in xrange(len(neuropil_smoothed[seq_idx][0, :]))])
             corrected_timecourse = raw_timecourse - correction_factors + \
                 np.nanmean(correction_factors)
-            corrected_timecourses.append(corrected_timecourse)
-        sequence_signals.append(corrected_timecourses)
+            sequence_signals.append(corrected_timecourse)
+        sequence_signals.append(np.array(corrected_signals))
     return sequence_signals
--- sima/neuropil_test.py
+++ sima/neuropil_test.py
@@ -5,16 +5,18 @@ import matplotlib.pyplot as plt
 
 from pudb import set_trace
 
-TEST_DIR = '/data/Nathan/2photon/acuteRemapping/nd113/12102014/ctxA-002/ctxA-002.sima'
-CHANNEL = 'Ch2'
-LABEL = 'rois'
+# TEST_DIR = '/data/Nathan/2photon/acuteRemapping/nd113/12102014/ctxA-002/ctxA-002.sima'
+# CHANNEL = 'Ch2'
+# LABEL = 'rois'
 
-imSet = ImagingDataset.load(TEST_DIR)
+# imSet = ImagingDataset.load(TEST_DIR)
 
-raw_signals = imSet.signals(channel=CHANNEL)[LABEL]['raw']
+# raw_signals = imSet.signals(channel=CHANNEL)[LABEL]['raw']
 
-subtracted_signals = subtract_neuropil(
-    imSet, imSet._resolve_channel(CHANNEL), LABEL, min_distance=5,
-    grid_dim=(3, 3), contamination_ratio = 1)
+# subtracted_signals = subtract_neuropil(
+#     imSet, imSet._resolve_channel(CHANNEL), LABEL, min_distance=5,
+#     grid_dim=(3, 3), contamination_ratio = 1)
+
+subtract_neuropil(None, 1, 'rois', min_distance=5, grid_dim=(3, 3))
 
 set_trace()
